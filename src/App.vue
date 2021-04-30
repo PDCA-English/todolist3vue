c974ace4604044c65818618ecbbe8346a1fef9c5
Takuya <r28.6sa@gmail.com>
2021-04-30 23:13:11 +0900

    changing url to api

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@ export default {
   },
   methods: {
     async getContact() {
-      const resData = await axios.get("http://127.0.0.1:8000/api/contact/");
+      const resData = await axios.get("https://blooming-caverns-24754.herokuapp.com/api/contact/");
       this.contactLists = resData.data.data;
     },
     async insertContact() {
@@ -61,7 +61,7 @@ export default {
         name: this.newName,
         email: this.newEmail,
       };
-      await axios.post("http://127.0.0.1:8000/api/contact/", sendData);
+      await axios.post("https://blooming-caverns-24754.herokuapp.com/api/contact/", sendData);
       await this.getContact();
     },
     async updateContact(id, name, email) {
@@ -69,11 +69,11 @@ export default {
         name: name,
         email: email,
       };
-      await axios.put("http://127.0.0.1:8000/api/contact/" + id, sendData);
+      await axios.put("https://blooming-caverns-24754.herokuapp.com/api/contact/" + id, sendData);
       await this.getContact();
     },
     async deleteContact(id) {
-      await axios.delete("http://127.0.0.1:8000/api/contact/" + id);
+      await axios.delete("https://blooming-caverns-24754.herokuapp.com/api/contact/" + id);
       await this.getContact();
     },
   },
